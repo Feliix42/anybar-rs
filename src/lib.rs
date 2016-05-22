d21325dcbfc789ca92a42a04c68b7d63fcf4c7f4
Felix Wittwer <dev@felixwittwer.de>
2016-05-22 19:46:16 +0200

    Implement Anybar struct and constructor

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,62 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::net;
+
+pub struct Anybar {
+    /// The UDP Port the Anybar is connected to
+    pub port: u16,
+    /// The last color that has been set
+    pub color: Option<Color>,
+}
+
+pub enum Color {
+    White,
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Cyan,
+    Blue,
+    Purple,
+    Black,
+    Question,
+    Exclamation,
+}
+
+impl Anybar {
+    pub fn new(port: u16) -> Anybar {
+        //TODO check if port is valid
+        Anybar{port:port, color:None}
+    }
+
+    fn parse_color(color: Color) -> Vec<u8> {
+        use Color::*;
+        let col = match color {
+            White       => "white",
+            Red         => "red",
+            Orange      => "orange",
+            Yellow      => "yellow",
+            Green       => "green",
+            Cyan        => "cyan",
+            Blue        => "blue",
+            Purple      => "purple",
+            Black       => "black",
+            Question    => "question",
+            Exclamation => "exclamation",
+        };
+
+        let mut parsed: Vec<u8> = Vec::new();
+        parsed.extend(col.as_bytes()
+                         .iter());
+        parsed
+    }
+
+    pub fn set_color(&self, color: Color) {
+        let message = Anybar::parse_color(color);
+        // TODO
+    }
+}
+
+impl Default for Anybar {
+    fn default() -> Anybar {
+        Anybar{port:1738, color:None}
     }
 }
