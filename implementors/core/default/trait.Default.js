acd10d56eb0e6b5d8a1d935f1b7f06db418b510e
Felix Wittwer <dev@felixwittwer.de>
2016-09-08 19:36:24 +0200

    Update docs to version 0.1.2

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['anybar'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='anybar/struct.Anybar.html' title='anybar::Anybar'>Anybar</a>",];
+implementors["anybar"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='anybar/struct.Anybar.html' title='anybar::Anybar'>Anybar</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
